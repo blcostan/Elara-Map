8319469e4e927ac5abaab57cb798f5e84d7e2dcf
blcostan <blcostan@ncsu.edu>
2026-07-24 20:59:45 -0400

    Fix basemap URL for CartoDB Dark Matter layer

--- map.js
+++ map.js
@@ -3,8 +3,8 @@ const map = L.map('map', {
     zoomControl: false
 }).setView([35.775, -78.650], 13);
 
-// Add CartoDB Dark Matter No-Labels basemap (Fixed URL)
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+// Add CartoDB Dark Matter No-Labels basemap with correct URL for hidden labels/roads
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
     maxZoom: 20,
     subdomains: 'abcd',
     attribution: '&copy; OpenStreetMap contributors &copy; CARTO'
